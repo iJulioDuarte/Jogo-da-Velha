e38ed3942af9fec2fbc29f77d6129403197714ae
Julio Cesar Vicentin Duarte <101108255+iJulioDuarte@users.noreply.github.com>
2022-05-26 17:41:45 -0300

    Add files via upload

---
+++ Jogo da velha.py
@@ -0,0 +1,162 @@
+jogadas = 0
+linha = 0
+coluna = 0
+soma = 0
+vez = 0
+velha = [
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "]
+]
+
+def exibirtab():  
+    global velha
+    print("    0   1   2")
+    print("0:  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2])
+    print("   -----------")
+    print("1:  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2])
+    print("   -----------")
+    print("2:  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2])
+
+
+while True:
+    exibirtab()
+    if vez >=9:
+        break
+    elif vez % 2 == 0:
+        linha, coluna = map(int, input("Digite o valor da linha e da coluna: ").split())
+        while velha[linha][coluna] == 'X' or velha[linha][coluna] == 'O':
+            print("Está posição ja está ocupada, tente outra:")
+            linha, coluna = map(int, input("Digite o valor da linha e da coluna: ").split())
+        velha[linha][coluna] = "X"
+        vez +=1
+        print()
+
+    #verific
+    for i in range(3):
+        for j in range(3):
+            if velha[i][j] == 'X':
+                soma += 1
+            if soma == 3:
+                exibirtab()
+                print("X ganhou!")
+                vez = 10
+        soma = 0
+    if vez >=9:
+        break
+    soma = 0
+
+    for i in range(3):
+        soma = 0
+        for j in range(3):
+            if velha[j][i] == 'X':
+                soma += 1
+            if soma == 3:
+                exibirtab()
+                print("X ganhou!")
+                vez = 10
+        soma = 0
+    if vez >=9:
+        break
+    soma = 0
+
+    for i in range(3):
+        if velha[i][i] == 'X':
+            soma += 1
+        if soma == 3:
+            exibirtab()
+            print("X ganhou!")
+            vez = 10
+    soma = 0
+    if vez >=9:
+        break
+    soma = 0
+    
+    for i in range(3):
+        for j in range(2-i, -1, -2):
+
+            if velha[i][j] == 'X':
+                soma += 1
+            if j == 0 and i == 0:
+                if velha[i][j] == 'X':
+                    soma = 1
+        if soma == 3:
+            exibirtab()
+            print("X ganhou!")
+            vez = 10
+    soma = 0
+    if vez >=9:
+        break
+
+    #fimverific
+    else:
+        exibirtab()
+        linha, coluna = map(int, input("Digite o valor da linha e da coluna: ").split())
+        while velha[linha][coluna] == 'X' or velha[linha][coluna] == 'O':
+            print("Está posição ja está ocupada, tente outra!")
+            linha, coluna = map(int, input("Digite o valor da linha e da coluna: ").split())
+        velha[linha][coluna] = "O"
+        vez+=1
+        print()
+
+        for i in range(3):
+            for j in range(3):
+                if velha[i][j] == 'O':
+                    soma += 1
+                if soma == 3:
+                    exibirtab()
+                    print("O ganhou!")
+                    vez = 10
+            soma = 0
+        if vez >=9:
+            break
+
+        for i in range(3):
+            for j in range(3):
+                if velha[j][i] == 'O':
+                    soma += 1
+                if soma == 3:
+                    exibirtab()
+                    print("O ganhou!")
+                    vez = 10
+            soma = 0
+        if vez >=9:
+            break
+        soma = 0
+
+        for i in range(3):
+            if velha[i][i] == 'O':
+                soma += 1
+            if soma == 3:
+                exibirtab()
+                print("O ganhou!")
+                vez = 10
+        soma = 0
+        if vez >=9:
+            break
+        soma = 0
+        
+        for i in range(3):
+            for j in range(2-i, -1, -2):
+                if velha[i][j] == 'O':
+                    soma += 1
+                if j == 0 and i == 0:
+                    if velha[i][j] == 'O':
+                        soma = 1
+            if soma == 3:
+                exibirtab()
+                print("O ganhou!")
+                vez = 10
+        soma = 0
+        if vez >=9:
+            break
+        #fimverific
+
+for i in range(3):
+    for j in range(3):
+        if velha[i][j] == 'X' or velha[i][j] == 'O':
+            soma += 2
+if soma > 16:
+    print("DEU VELHA!")
+    exibirtab()
+    
